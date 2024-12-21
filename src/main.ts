464d4e8e64ca14aaadbfae5b7f1663e7ffe15a2b
Muhammad Waqar <waqarilyas.dev@gmail.com>
2024-12-21 23:02:10 +0500

    feat: integrate Swagger for API documentation and update package dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('DMS API')
+    .setDescription('')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
